fdbaa74da8f322d5ed00e80358b70e65b71fd249
andreasbloomquist <andreasbloomquist@woboinc.com>
2015-05-14 20:44:12 -0700

    Add form to take new phrases and delete

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.static(__dirname + '/node_modules'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var phrases = [
-	{id: 0, word:"airplane", definition:"A vehicle to transport people via the sky"},
-	{id: 1, word: "car", definition: "A vehicle for transportation via roads"},
-	{id: 2, word: "train", definition: "A method of transportation using rails"}
+	{id: 0, word:"npm", definition:"Node Package Manager, a management system for Node packages"},
+	{id: 1, word: "Relational Database", definition: "SQL db, a database that keeps data in separate but related tables"},
+	{id: 2, word: "Tuna", definition: "A type of fish, do you even sushi bro?"}
 ];
 
 app.get("/", function (req, res){
@@ -23,6 +23,21 @@ app.get("/", function (req, res){
 app.get("/phrases", function(req, res){
 	console.log(__dirname);
 	res.send(JSON.stringify(phrases));
+});
+
+app.post("/phrases", function(req, res){
+	var newPhrase = req.body;
+	newPhrase.id = phrases[phrases.length - 1].id + 1;
+	phrases.push(newPhrase);
+	res.send(JSON.stringify(newPhrase));
+});
+
+app.delete("/phrases/:id", function(req, res){
+	var targetId = parseInt(req.params.id);
+	var targetItem = _.findWhere(phrases, {id: targetId});
+	var index = phrases.indexOf(targetItem);
+	phrases.splice(index, 1);
+	res.send(JSON.stringify(targetItem));
 });
 
 app.listen(3000, function (){
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,20 @@
 $(function(){
   // get and render the phrases
   Phrase.all();
+  View.init();  
 });
 
 //View object
 function View() {};
+
+View.init = function(){
+  $('#new-phrase').on('submit', function(e){
+    e.preventDefault();
+    var phraseParams = $(this).serialize();
+    Phrase.create(phraseParams);
+  });
+};
+
 View.render = function(items, parentId, templateId) {
   // render a template
   var template = _.template($("#" + templateId).html());
@@ -19,4 +29,23 @@ Phrase.all = function() {
 		var x = JSON.parse(res);
 		View.render(x, "phrases-ul", "phrases-template");
 	});
+};
+
+Phrase.create = function(phraseParams) {
+  $.post("/phrases", phraseParams).done(function(res){
+    Phrase.all();
+  }).done(function(res){
+    $('#new-phrase')[0].reset();
+  });
+};
+
+Phrase.delete = function(phrase){
+  var wordId = $(phrase).data().id;
+  $.ajax({
+    url: '/phrases/' + wordId,
+    type: 'DELETE',
+    success: function(res){
+      Phrase.all()
+    }
+  });
 };
